89e2d2d7c72ad48aa7e55af17adb6f5dbaeb6334
leanalarcon <alarcon.l@live.com>
2021-04-30 11:38:18 -0300

    All the files corresponding to Localized Climate, first version, have been uploaded.

---
+++ javascript.js
@@ -0,0 +1,44 @@
+
+const api_key = 'cac398ffae07e7fda830f581c321af53';
+
+const fetchData = position => {
+    const { latitude, longitude } = position.coords;
+    fetch('http://api.openweathermap.org/data/2.5/weather?units=metric&lat=' + latitude + '&lon=' + longitude + '&appid=' + api_key)
+        .then(response => response.json())
+        .then(data => setWeatherData(data))
+}
+
+const setWeatherData = data => {
+    console.log(data);
+    const weatherData = {
+        location: data.name,
+        description: data.weather[0].description,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: data.main.temp,
+        date: getDate(),
+    }
+
+    Object.keys(weatherData).forEach( key => {
+        document.getElementById(key).textContent = weatherData[key];
+    });
+
+    cleanUp();
+}
+
+const cleanUp = () => {
+    let container = document.getElementById('container');
+    let loader = document.getElementById('loader');
+
+    loader.style.display = 'none';
+    container.style.display = 'flex';
+}
+
+const getDate = () => {
+    let date = new Date();
+    return `${date.getDate()}-${( '0' + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`;
+}
+
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData);
+}
